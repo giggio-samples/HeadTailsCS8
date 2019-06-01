9d8a5cd510533787dc7eafcb86bc09685ce33139
Giovanni Bassi <giggio@giggio.net>
2019-05-31 22:31:27 -0300

    First

---
+++ HeadTailsCS8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace HeadTailsCS8
+{
+    class Program
+    {
+        static void Main() => WriteLine($"Total: {Soma(new[] { 1, 2, 3, 4, 5 })}");
+
+        static int Soma(in ReadOnlySpan<int> ns) =>
+            ns switch
+            {
+                //ReadOnlySpan<int>.Empty => 0, // we would love to do this but the compile can't yet understand, see https://github.com/dotnet/csharplang/issues/1431#issuecomment-497901047
+                var x when x.IsEmpty => 0,
+                var (head, tail) => head + Soma(tail)
+            };
+    }
+
+    static class ArrayDeconstruction
+    {
+        public static void Deconstruct<T>(in this ReadOnlySpan<T> x, out T head, out ReadOnlySpan<T> tail)
+        {
+            head = x[0];
+            tail = x.Slice(1);
+        }
+    }
+}
